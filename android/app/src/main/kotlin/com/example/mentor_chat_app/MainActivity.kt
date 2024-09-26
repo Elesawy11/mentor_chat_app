f64f9ac1cd497c93d47d4b94e146350333429502
elesawy2 <elesawy928@gmail.com>
2024-09-26 16:12:38 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mentor_chat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mentor_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MentorChatApp(),
+  );
+}
+
+class MentorChatApp extends StatelessWidget {
+  const MentorChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
